e3de8c8a3534ecbc8a80c14ec40e354f0104910d
halilcakar <hcakar.1992@gmail.com>
2021-10-19 19:34:29 +0300

    Adding a Meals component

---
+++ src/components/Meals/AvaiableMeals.jsx
@@ -0,0 +1,39 @@
+import classes from './AvaiableMeals.module.css';
+const DUMMY_MEALS = [
+  {
+    id: 'm1',
+    name: 'Sushi',
+    description: 'Finest fish and veggies',
+    price: 22.99,
+  },
+  {
+    id: 'm2',
+    name: 'Schnitzel',
+    description: 'A german specialty!',
+    price: 16.5,
+  },
+  {
+    id: 'm3',
+    name: 'Barbecue Burger',
+    description: 'American, raw, meaty',
+    price: 12.99,
+  },
+  {
+    id: 'm4',
+    name: 'Green Bowl',
+    description: 'Healthy...and green...',
+    price: 18.99,
+  },
+];
+
+const AvaiableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+
+  return (
+    <section className={classes.meals}>
+      <ul>{mealsList}</ul>
+    </section>
+  );
+};
+
+export default AvaiableMeals;
---
+++ src/components/Meals/Meals.jsx
@@ -0,0 +1,13 @@
+import MealsSummary from './MealsSummary';
+import AvaiableMeals from './AvaiableMeals';
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvaiableMeals />
+    </>
+  );
+};
+
+export default Meals;
