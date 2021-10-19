f6f3257c10732034d92a368766fae010a173b43d
halilcakar <hcakar.1992@gmail.com>
2021-10-19 19:08:26 +0300

    Create HeaderCartButton

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,13 +1,14 @@
 import classes from './Header.module.css';
 
 import mealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = () => {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="A table full of delicious food" />
